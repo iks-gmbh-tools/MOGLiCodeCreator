2bcbf9a108517680db2f39346969f10899400525
Reik Oberrath <r.oberrath@iks-gmbh.com>
2013-02-05 18:33:32 +0100

    method testCallMaven corrected

--- application/src/test/java/com/iksgmbh/moglicc/build/helper/MavenExecutorBuildTest.java
+++ application/src/test/java/com/iksgmbh/moglicc/build/helper/MavenExecutorBuildTest.java
@@ -13,23 +13,23 @@ import com.iksgmbh.moglicc.build.helper.MavenExecutor.MavenData;
 import com.iksgmbh.moglicc.build.test.ApplicationTestParent;
 
 public class MavenExecutorBuildTest extends ApplicationTestParent {
-	
+
 	private MavenExecutor mavenExecutor;
 
 	@Before
 	public void setup() {
 		super.setup();
-		MavenData mavenData = new MavenData("compile", (new MOGLiReleaseBuilder()).getMavenRootDir(), 
+		MavenData mavenData = new MavenData("compile", (new MOGLiReleaseBuilder()).getMavenRootDir(),
 				new File(PROJECT_ROOT_DIR + "../global"));
 		mavenExecutor = new MavenExecutor(mavenData);
 	}
-	
+
 	@Test
 	public void testCallMaven() {
 		String mavenResult = mavenExecutor.callMaven();
 		assertNotNull(mavenResult);
 		System.out.println(mavenResult);
-		assertTrue("Unexpected mavenResult.", mavenResult.contains("[INFO] [compiler:compile {execution: default-compile}]"));
-		assertTrue("Unexpected mavenResult.", mavenResult.contains("[INFO] BUILD SUCCESSFUL"));
+		assertTrue("Unexpected mavenResult: " + mavenResult, mavenResult.contains("[INFO] [compiler:compile {execution: default-compile}]"));
+		assertTrue("Maven Build not successful: " + mavenResult, mavenResult.contains("[INFO] BUILD SUCCESSFUL"));
 	}
 }
