766f5df1937c25ecd42e9672e144d57548d55d0c
iks-github <github@iks-gmbh.com>
2013-10-16 09:47:04 +0200

    travis-ci changes

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,2 +1,4 @@
+/usr/bin/env sh
+
 mvn install:install-file -DgroupId=cpsuite -DartifactId=cpsuite -Dpackaging=jar -Dversion=1.2.6 -Dfile=url://http://johanneslink.net/downloads/cpsuite/cpsuite-1.2.6.jar -DgeneratePom=true -U 
 mvn install -Dmaven.test.skip=true
