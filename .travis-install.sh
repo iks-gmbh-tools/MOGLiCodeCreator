ea1008e7377a894b3318c747c555fb1368a4be77
iks-github <github@iks-gmbh.com>
2013-10-16 10:08:49 +0200

    travis-ci changes/bugfixes

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 mvn install:install-file -DgroupId=cpsuite -DartifactId=cpsuite -Dpackaging=jar -Dversion=1.2.6 -Dfile=url://http://johanneslink.net/downloads/cpsuite/cpsuite-1.2.6.jar -DgeneratePom=true -U 
-mvn install -Dmaven.test.skip=true --file ./parent/pom.xml --fail-never
+mvn clean install -Dmaven.test.skip=true --file ./parent/pom.xml --fail-never
