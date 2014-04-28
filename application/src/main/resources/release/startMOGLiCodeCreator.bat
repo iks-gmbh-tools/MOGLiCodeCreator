7e0c76265964de7336e43e7b2b74f5bd9177f954
Reik Oberrath <R.Oberrath@iks-gmbh.com>
2014-04-28 17:28:47 +0200

    MOGLiCC start scripts refactored

--- application/src/main/resources/release/startMOGLiCodeCreator.bat
+++ application/src/main/resources/release/startMOGLiCodeCreator.bat
@@ -1,34 +1,24 @@
 echo off
 set "workspaceDir=%~1"
 set classpath=".\lib\*;.\lib\plugins\*"
-set JAVABIN=%JAVA_HOME%/bin
-
 
 if "%JAVA_HOME%" == "" goto Java_Home_Not_Set
-if not exist "%JAVA_HOME%\bin\java.exe" goto Java_EXE_Not_FOUND_IN_JAVA_HOME
-echo %JAVA_HOME%\bin\java
-"%JAVA_HOME%\bin\java" -cp %classpath% com.iksgmbh.moglicc.MOGLiCodeCreator %workspaceDir%
+if not exist "%JAVA_HOME%\java.exe" goto Java_EXE_Not_FOUND_IN_JAVA_HOME
+echo java.exe found in %JAVA_HOME%
+"%JAVA_HOME%\java" -cp %classpath% com.iksgmbh.moglicc.MOGLiCodeCreator %workspaceDir%
 goto End
-
-
-:Java_Home_Not_OK
-if not exist %JAVABIN%\java.exe goto Java_Bin_Not_OK
-%JAVABIN%\java -cp %classpath% com.iksgmbh.moglicc.MOGLiCodeCreator %workspaceDir%
-goto End
-
 
 :Java_Home_Not_Set
-echo environment varaiable JAVA_HOME not set
-goto Java_Home_Not_OK
-
+echo "Problem: no JVM available because environment variable JAVA_HOME is not set"
+goto End
 
 :Java_EXE_Not_FOUND_IN_JAVA_HOME
-echo Java executable not found in "%JAVA_HOME%\bin"
-goto Java_Home_Not_OK
+if not exist %JAVA_HOME%\bin\java.exe goto Java_Home_Bin_Does_Not_Exist
+echo java.exe found in %JAVA_HOME%\bin
+%JAVA_HOME%\bin\java -cp %classpath% com.iksgmbh.moglicc.MOGLiCodeCreator %workspaceDir%
+goto End
 
-
-:Java_Bin_Not_OK
-echo Java executable not found in %JAVABIN%\java
-
+:Java_Home_Bin_Does_Not_Exist
+echo "Problem: no JVM available because no java executable was found in %JAVA_HOME%"
 
 :End
