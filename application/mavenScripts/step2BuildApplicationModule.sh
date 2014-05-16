8535e4e9307ceadf9c922c5d6b69d34ed741d400
Reik Oberrath <R.Oberrath@iks-gmbh.com>
2014-05-16 17:25:38 +0200

    cpsuite project added and application/mavenscripts updated

--- application/mavenScripts/step2BuildApplicationModule.sh
+++ application/mavenScripts/step2BuildApplicationModule.sh
@@ -15,7 +15,7 @@ echo "*     ***********************************************************"
 echo "*     ***********************************************************"
 echo "*     ***********************************************************"
 echo "*     *****                                                 *****"
-echo "*     *****                      Step 1:                    *****"
+echo "*     *****                      Step 2:                    *****"
 echo "*     *****                  PREPARE TESTING                *****"
 echo "*     *****     mvn clean install on application module     *****"
 echo "*     *****                                                 *****"
@@ -28,10 +28,26 @@ echo "*"
 echo "*"
 
 cd ..
-mvn clean install
+mvn clean install -Dtest=BuildTestExecutor
 
 echo "*"
 echo "*"
+echo "*     ***********************************************************"
+echo "*     ***********************************************************"
+echo "*     *                                                         *"
+echo "*     *                  ATTENTION:                             *"
+echo "*     *                                                         *"
+echo "*     * Make sure that the maven settings are valid defined in  *"
+echo "*     * application/src/main/resources/build.properties         *"
+echo "*     *                                                         *"
+echo "*     * If not, some tests in this step and the following steps *"
+echo "*     * will fail!                                              *"
+echo "*     *                                                         *"
+echo "*     ***********************************************************"
+echo "*     ***********************************************************"
+echo "*"
+echo "*"
+
 echo "Press [Enter] to continue..."
 read enter
 
