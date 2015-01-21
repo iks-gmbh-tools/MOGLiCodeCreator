ac3b768dbe4513950954e6655e901d43e06e4024
Reik Oberrath <R.Oberrath@iks-gmbh.com>
2015-01-21 14:28:13 +0100

    Finishing changes for Release 1.5.0

--- cpsuite/src/main/java/org/junit/extensions/cpsuite/AcceptAllTester.java
+++ cpsuite/src/main/java/org/junit/extensions/cpsuite/AcceptAllTester.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * @author Johannes Link (business@johanneslink.net)
  * 
@@ -23,4 +24,31 @@ public class AcceptAllTester implements ClassTester {
 		return true;
 	}
 
+=======
+/*
+ * @author Johannes Link (business@johanneslink.net)
+ * 
+ * Published under Apache License, Version 2.0 (http://apache.org/licenses/LICENSE-2.0)
+ */
+package org.junit.extensions.cpsuite;
+
+public class AcceptAllTester implements ClassTester {
+
+	public boolean acceptClassName(String className) {
+		return true;
+	}
+
+	public boolean acceptInnerClass() {
+		return true;
+	}
+
+	public boolean acceptClass(Class<?> clazz) {
+		return true;
+	}
+
+	public boolean searchInJars() {
+		return true;
+	}
+
+>>>>>>> originReikOberrath/master
 }
